d50a9844bc3fe625780468c425bb08a3ca3ba580
RubbishPanda <30775997+porkandbeans@users.noreply.github.com>
2022-10-01 16:18:35 +1300

    attempt to fix qb-phone/html/js/racing.js

--- resources/[qb]/qb-phone/html/js/racing.js
+++ resources/[qb]/qb-phone/html/js/racing.js
@@ -97,11 +97,11 @@ function SetupRaces(Races) {
                 }
             }
             var Racers = GetAmountOfRacers(race.RaceData.Racers);
-            // var element = '<div class="racing-race" id="raceid-'+i+'"> <span class="race-name"><i class="fas fa-flag-checkered"></i> '+race.RaceData.RaceName+'</span> <span class="race-track">'+Locked+'</span> <div class="race-infomation"> <div class="race-infomation-tab" id="race-information-laps">'+LapLabel+'</div> <div class="race-infomation-tab" id="race-information-distance">'+race.RaceData.Distance+' m</div> <div class="race-infomation-tab" id="race-information-player"><i class="fas fa-user"></i> '+Racers+'</div> </div> '+Buttons+' </div> </div>';
-            var element = '<div class="racing-race" id="i"> <span class="race-name"><i class="fas fa-flag-checkered"></i> '+race.RaceData.RaceName+'</span> <span class="race-track">'+Locked+'</span> <div class="race-infomation"> <div class="race-infomation-tab" id="race-information-laps">'+LapLabel+'</div> <div class="race-infomation-tab" id="race-information-distance">'+race.RaceData.Distance+' m</div> <div class="race-infomation-tab" id="race-information-player"><i class="fas fa-user"></i> '+Racers+'</div> </div> '+Buttons+' </div> </div>';
+            var element = '<div class="racing-race" id="raceid-'+i+'"> <span class="race-name"><i class="fas fa-flag-checkered"></i> '+race.RaceData.RaceName+'</span> <span class="race-track">'+Locked+'</span> <div class="race-infomation"> <div class="race-infomation-tab" id="race-information-laps">'+LapLabel+'</div> <div class="race-infomation-tab" id="race-information-distance">'+race.RaceData.Distance+' m</div> <div class="race-infomation-tab" id="race-information-player"><i class="fas fa-user"></i> '+Racers+'</div> </div> '+Buttons+' </div> </div>';
+            // var element = '<div class="racing-race" id="i"> <span class="race-name"><i class="fas fa-flag-checkered"></i> '+race.RaceData.RaceName+'</span> <span class="race-track">'+Locked+'</span> <div class="race-infomation"> <div class="race-infomation-tab" id="race-information-laps">'+LapLabel+'</div> <div class="race-infomation-tab" id="race-information-distance">'+race.RaceData.Distance+' m</div> <div class="race-infomation-tab" id="race-information-player"><i class="fas fa-user"></i> '+Racers+'</div> </div> '+Buttons+' </div> </div>';
             $(".racing-races").append(element);
-            // $("#raceid-"+i).data('RaceData', race);
-            $(i).data('RaceData', race);
+            $("#raceid-"+i).data('RaceData', race);
+            // $(i).data('RaceData', race);
             if (!race.RaceData.Started) {
                 $("#raceid-"+i).css({"border-bottom-color":"#34b121"});
             } else {
@@ -134,10 +134,11 @@ $(document).on('click', '#join-race', function(e){
                     }), function(IsBusy){
                         if (!IsBusy) {
                             $.post('https://qb-phone/JoinRace', JSON.stringify({
-                                RaceData: {
-                                    ...Data,
-                                    RaceId
-                                },
+                            RaceData: Data,
+                            // RaceData: {
+                                //     ...Data,
+                                //     RaceId
+                                // },
                             }));
                             $.post('https://qb-phone/GetAvailableRaces', JSON.stringify({}), function(Races){
                                 SetupRaces(Races);
